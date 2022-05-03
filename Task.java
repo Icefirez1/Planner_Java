ab79f2ea5f0665f5b2288d6b546ad4d9f788798e
Al <alpagar9@gmail.com>
2022-05-03 13:46:11 -0400

    Creating Task class

---
+++ Task.java
@@ -0,0 +1,62 @@
+
+import java.util.Date;
+import java.text.DateFormat;
+public class Task
+{
+
+    //Chore and due date of task
+    private String chore; 
+    private Date dueDate;
+    /**
+     * Constructors
+     */
+    public Task()
+    {
+        this.chore = "none";
+        this.dueDate = new Date();
+    }
+    public Task(String toDo)
+    {
+        this.chore = toDo;
+        this.dueDate = new Date();
+    }
+    //figure this out some other day
+    // Im trying to implement so that they can add the task to another day
+    // mabye instead of them typing I have them do it through like a wheel
+    // public Task(String toDo, String dateDue)
+    // {
+    //     this.chore = toDo;
+    //     this.dueDate = DateFormat.pa
+    // }
+
+
+
+    /**
+     * Changes task
+     * @param newTask
+     */
+    public void changeTask(String newTask)
+    {
+        this.chore = newTask;
+    }
+    /**
+     * Changes Due Date
+     * @param newDate
+     */
+    public void changeDate(Date newDate)
+    {
+        this.dueDate = newDate;
+    }
+
+
+    public String toString()
+    {
+        return chore.concat(" ").concat(dueDate.toString());
+    }
+    public static void main(String[] args)
+    {
+        
+        Task stuff = new Task("yuh"); 
+        System.out.println(stuff);
+    }
+}
